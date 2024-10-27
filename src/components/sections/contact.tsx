329ff1d95de866abf2d0df2d963328f17f7a5783
Affan Noviananda Putra <affannovian@gmail.com>
2024-10-27 22:24:40 +0700

    feat: adding 3d calculator and smooth scroll

---
+++ src/components/sections/contact.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+const ContactSection = () => {
+    return (
+        <section id="contact" className={cn("relative min-h-screen max-w-7xl mx-auto")}></section>
+    )
+}
+
+export default ContactSection;
---
+++ src/components/sections/projects.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+const ProjectSection = () => {
+  return (
+    <section id="projects" className={cn("relative max-w-7xl mx-auto md:h-[130vh]")}></section>
+  );
+};
+
+export default ProjectSection;
